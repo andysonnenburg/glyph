807ba96d1c21c8d913060706ff641b1bb75c4a58
Andy Sonnenburg <andy22286@gmail.com>
2012-01-30 07:13:50 -0600

    Chance ConstProp module to be syntactically valid

--- src/Language/Glyph/Hoopl/ConstProp.hs
+++ src/Language/Glyph/Hoopl/ConstProp.hs
@@ -1,9 +1,3 @@
 module Language.Glyph.Hoopl.ConstProp
-       ( constProp
+       (
        ) where
-
-type ConstFact = Map Var (WithTop Lit)
-
-constLattice :: DataflowLattice ConstFact
-constLattice =
-  DataflowLattice { 
